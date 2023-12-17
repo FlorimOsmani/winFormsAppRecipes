9827eb601857c72b1f24cf74c8085ae790caf3c2
Florim <florim409@gmail.com>
2023-12-17 19:34:02 +0100

    Database creation added

--- WinFormsReceptenBoek/DatabaseManager.cs
+++ WinFormsReceptenBoek/DatabaseManager.cs
@@ -24,14 +24,28 @@ namespace WinFormsReceptenBoek
             _context = context;
         }
 
-        /*private string connectionString = @"Data Source=..\..\..\Files\ReceptenDatabase.db;Version=3;";*/
-
-        /*public void InitializeDatabase()
+        public void InitializeDatabase()
         {
             if (!File.Exists(@"..\..\..\Files\ReceptenDatabase.db"))
             {
-                SQLiteConnection.CreateFile(@"..\..\..\Files\ReceptenDatabase.db");
+                CreateSQLiteDatabase();
             }
+
+            _context.Database.EnsureCreated();
+        }
+
+        private void CreateSQLiteDatabase()
+        {
+            // Get the directory where the application is running
+            string baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+
+            // Combine the base directory with the relative path to the database file
+            string databasePath = Path.Combine(baseDirectory, @"Files\ReceptenDatabase.db");
+
+            string connectionString = $"Data Source={databasePath};Version=3;";
+
+            // Ensure the directory exists before creating the file
+            Directory.CreateDirectory(Path.GetDirectoryName(databasePath));
 
             using (SQLiteConnection connection = new SQLiteConnection(connectionString))
             {
@@ -43,57 +57,7 @@ namespace WinFormsReceptenBoek
                     command.ExecuteNonQuery();
                 }
             }
-        }*/
-
-        /*public void InitializeDatabase()
-        {
-            using (var context = new ApplicationDbContext())
-            {
-                context.Database.EnsureCreated();
-            }
-        }*/
-
-        public void InitializeDatabase()
-        {
-            _context.Database.EnsureCreated();
         }
-
-        /*public void VoegReceptToe(string naam, string ingrediënten, string instructies, string imgurLink)
-        {
-            using (SQLiteConnection connection = new SQLiteConnection(connectionString))
-            {
-                connection.Open();
-
-                string query = "INSERT INTO Recepten (Naam, Ingrediënten, Instructies, ImgurLink) VALUES (@Naam, @Ingrediënten, @Instructies, @ImgurLink)";
-
-                using (SQLiteCommand command = new SQLiteCommand(query, connection))
-                {
-                    command.Parameters.AddWithValue("@Naam", naam);
-                    command.Parameters.AddWithValue("@Ingrediënten", ingrediënten);
-                    command.Parameters.AddWithValue("@Instructies", instructies);
-                    command.Parameters.AddWithValue("@ImgurLink", imgurLink);
-
-                    command.ExecuteNonQuery();
-                }
-            }
-        }*/
-
-        /*public void VoegReceptToe(string naam, string ingrediënten, string instructies, string imgurLink)
-        {
-            using (var context = new ApplicationDbContext())
-            {
-                var nieuwRecept = new Recept
-                {
-                    Naam = naam,
-                    Ingrediënten = ingrediënten,
-                    Instructies = instructies,
-                    ImgurLink = imgurLink
-                };
-
-                context.Recepten.Add(nieuwRecept);
-                context.SaveChanges();
-            }
-        }*/
 
         public void VoegReceptToe(string naam, string ingrediënten, string instructies, string imageLink)
         {
@@ -109,22 +73,6 @@ namespace WinFormsReceptenBoek
             _context.SaveChanges();
         }
 
-        /*public void VerwijderRecept(int receptID)
-        {
-            using (SQLiteConnection connection = new SQLiteConnection(connectionString))
-            {
-                connection.Open();
-
-                string query = "DELETE FROM Recepten WHERE ID = @ReceptID";
-                using (SQLiteCommand command = new SQLiteCommand(query, connection))
-                {
-                    command.Parameters.AddWithValue("@ReceptID", receptID);
-
-                    command.ExecuteNonQuery();
-                }
-            }
-        }*/
-
         public void VerwijderRecept(int receptID)
         {
             var recept = _context.Recepten.Find(receptID);
@@ -134,28 +82,6 @@ namespace WinFormsReceptenBoek
                 _context.SaveChanges();
             }
         }
-
-        /*public void WerkReceptBij(int receptID, string nieuweNaam, string nieuweIngrediënten, string nieuweInstructies, string nieuweImgurLink)
-        {
-            // Voeg de Imgur-link toe aan de UPDATE-query
-            string query = "UPDATE Recepten SET Naam = @NieuweNaam, Ingrediënten = @NieuweIngrediënten, Instructies = @NieuweInstructies, ImgurLink = @NieuweImgurLink WHERE ID = @ReceptID";
-
-            using (SQLiteConnection connection = new SQLiteConnection(connectionString))
-            {
-                connection.Open();
-
-                using (SQLiteCommand command = new SQLiteCommand(query, connection))
-                {
-                    command.Parameters.AddWithValue("@ReceptID", receptID);
-                    command.Parameters.AddWithValue("@NieuweNaam", nieuweNaam);
-                    command.Parameters.AddWithValue("@NieuweIngrediënten", nieuweIngrediënten);
-                    command.Parameters.AddWithValue("@NieuweInstructies", nieuweInstructies);
-                    command.Parameters.AddWithValue("@NieuweImgurLink", nieuweImgurLink);
-
-                    command.ExecuteNonQuery();
-                }
-            }
-        }*/
 
         public void WerkReceptBij(int receptID, string nieuweNaam, string nieuweIngrediënten, string nieuweInstructies, string nieuweImageLink)
         {
@@ -171,43 +97,9 @@ namespace WinFormsReceptenBoek
             }
         }
 
-        /*public List<Recept> HaalAlleReceptenOp()
-        {
-            List<Recept> recepten = new List<Recept>();
-
-            using (SQLiteConnection connection = new SQLiteConnection(connectionString))
-            {
-                connection.Open();
-
-                string query = "SELECT * FROM Recepten";
-                using (SQLiteCommand command = new SQLiteCommand(query, connection))
-                {
-                    using (SQLiteDataReader reader = command.ExecuteReader())
-                    {
-                        while (reader.Read())
-                        {
-                            Recept recept = new Recept
-                            {
-                                ID = Convert.ToInt32(reader["ID"]),
-                                Naam = reader["Naam"].ToString(),
-                                Ingrediënten = reader["Ingrediënten"].ToString(),
-                                Instructies = reader["Instructies"].ToString(),
-                                ImgurLink = reader["ImgurLink"].ToString()
-                            };
-
-                            recepten.Add(recept);
-                        }
-                    }
-                }
-            }
-
-            return recepten;
-        }*/
-
         public List<Recept> HaalAlleReceptenOp()
         {
             return _context.Recepten.ToList();
         }
     }
-
 }
